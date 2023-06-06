43d53bc567fcb4f6974af791a3904009f097da81
DavidPK8 <alejodavicho8@gmail.com>
2023-06-06 09:15:51 -0500

    Creadas las variables privadas

--- src/figuras_3D.java
+++ src/figuras_3D.java
@@ -1,3 +1,10 @@
 public class figuras_3D {
+    private float lado;
+    private float base;
+    private float radio;
+    private float altura;
+    private double pi = 3.14;
+
+    //piramide esfera cilindro cubo
 
 }
